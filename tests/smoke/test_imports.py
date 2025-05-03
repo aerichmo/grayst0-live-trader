d0f346df008e9bcc917cef8d03dacc6887a8c913
Alec Richmo <alecrichmo@gmail.com>
2025-05-03 15:09:04 +0000

    ci: add smoke import test

---
+++ tests/smoke/test_imports.py
@@ -0,0 +1,11 @@
+"""
+Minimal “does-it-start?” smoke test suite.
+"""
+import importlib
+import pytest
+
+MODULES = ["requests", "yaml"]
+
+@pytest.mark.parametrize("module_name", MODULES)
+def test_can_import(module_name: str) -> None:
+    assert importlib.import_module(module_name)
